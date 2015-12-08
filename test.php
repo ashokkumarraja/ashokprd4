4850cb7c005eaed75dc9c49ca60062665fd29684
notionpress <notionpress@Bhargavas-Mac-mini.local>
2015-12-08 17:24:51 +0530

    sk

---
+++ test.php
@@ -0,0 +1,33 @@
+
+
+<?php
+
+for($i=1;$i<5;$i++)
+{
+	for($j=1;$j<=$i;$j++)
+	{
+
+		echo $j;
+		#$i++;
+		#$j++;
+		
+	
+		/*if($i==$j)
+		{
+			echo "1";
+		}
+		
+	
+		if($i>$j)
+		{
+			echo $i;
+		}
+		
+		#echo $i;
+*/
+}
+echo "<br>";
+}
+#echo $i;
+?>
+
