3627be293e27c1c66dbbf94465d06e70a7fd0eaa
notionpress <notionpress@Bhargavas-Mac-mini.local>
2015-12-08 17:45:48 +0530

    test change cammanded

--- test.php
+++ test.php
@@ -6,24 +6,11 @@ for($i=1;$i<5;$i++)
 {
 	for($j=1;$j<=$i;$j++)
 	{
-
-		echo $j;
-		#$i++;
-		#$j++;
-		
-	
-		/*if($i==$j)
-		{
-			echo "1";
-		}
-		
-	
-		if($i>$j)
-		{
-			echo $i;
-		}
-		
-		#echo $i;
+		/*
+		sdklaj
+		fdfkl
+		fdksafk
+content
 */
 }
 echo "<br>";
